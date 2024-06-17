8afcc5846437354ffcc45ae52ac2303fd78760d4
Marcel Dopita <m@rcel.cz>
2024-06-17 21:34:08 +0200

    Update

--- mqtt.py
+++ mqtt.py
@@ -3,10 +3,12 @@ from datetime import datetime, date, time, timezone
 import json
 import threading
 
-auto_off_timer = None
+timer_cabinet = None
+timer_toilet = None
 
 TOPIC_CABINET_DOOR = "zigbee2mqtt/living_room/cabinet/door"
 TOPIC_CABINET_LIGHT = "zigbee2mqtt/living_room/cabinet/light"
+TOPIC_TOILET_MOTION = "zigbee2mqtt/toilet/motion"
 
 def light(client, topic, state, brightness=None):
     print("light")
@@ -27,17 +29,30 @@ def handle_cabinet_door(client, contact, timer):
         if timer is not None:
             timer.cancel()
 
+def handle_room_motion(client, topic, timer):
+    light(client, topic, "ON", 255)
+    if timer is not None:
+        timer.cancel()
+    timer = threading.Timer(5 * 60, light, args=[client, topic, "OFF"])
+    timer.start()
+
 def on_connect(client, userdata, flags, reason_code, properties):
     print(f"Connected with result code {reason_code}")
     client.subscribe(TOPIC_CABINET_DOOR)
+    #client.subscribe(TOPIC_TOILET_MOTION)
 
 def on_message(client, userdata, msg):
-    global auto_off_timer
+    global timer_cabinet, timer_toilet
     print(msg.topic+" "+str(msg.payload))
     if msg.topic == TOPIC_CABINET_DOOR:
         payload = json.loads(msg.payload)
-        contact = payload["contact"]
-        handle_cabinet_door(client, contact, auto_off_timer)
+        if "contact" in payload:
+            handle_cabinet_door(client, payload["contact"], timer_cabinet)
+    elif msg.topic == TOPIC_TOILET_MOTION:
+        payload = json.loads(msg.payload)
+        if "occupancy" in payload:
+            if payload["occupancy"]:
+                handle_room_motion(client, TOPIC_TOILET_MOTION, timer_toilet)
 
 
 mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
