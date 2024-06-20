ce6b1504d9205decada609b9c95a97f43ec32fe2
Marcel Dopita <m@rcel.cz>
2024-06-21 00:29:06 +0200

    Door

--- light.py
+++ light.py
@@ -8,15 +8,19 @@ DIM_DURATION = 20
 class Light:
     sun_times = SunTimes()
 
-    def __init__(self, client, topic, off_time):
+    def __init__(self, client, topic, off_time, topic_door=None):
         self.client = client
         self.topic = topic
         self.off_time = off_time
+        self.topic_door = topic_door
         self.timer_dim = None
         self.timer_off = None
         self.state = None
         self.lux = 0
+        self.door_closed = False
+
         self.client.publish(f"{self.topic}/get", json.dumps({"state":""}))
+        self.client.publish(f"{self.topic_door}/get", json.dumps({"contact":False}))
 
     def __timers_cancel(self):
         if self.timer_dim is not None:
@@ -31,6 +35,7 @@ class Light:
         self.timer_off.start()
 
     def __timers_restart(self):
+        print("Restarting timers")
         self.__timers_cancel()
         self.__timers_start()
 
@@ -43,13 +48,25 @@ class Light:
     def update_lux(self, lux):
         self.lux = lux
 
+    # def update_door(self, door_closed):
+    #     print(f"Door closed: {door_closed}")
+    #     self.door_closed = door_closed
+    #     if self.topic_door is not None and not self.door_closed:
+    #         self.__timers_restart()
+    #     # TODO: Close will turn on the light early
+
     def on(self):
         # Turn on light only if it's not already on.
         # This is to prevent reseting the light settings (like brightness).
         if self.state is None or self.state == "OFF":
             control_light(self.client, self.topic, "ON", brightness=255)
-        # Light is on now, so start the off timers.
-        self.__timers_restart()
+        # Light is on now
+        # No timers if door is closed
+        if self.topic_door is not None and self.door_closed:
+            self.__timers_cancel()
+        # Start the off timers
+        else:
+            self.__timers_restart()
 
     def on_bathroom(self):
         # During night or low light
--- mqtt.py
+++ mqtt.py
@@ -6,26 +6,30 @@ TOPIC_CABINET_DOOR = "zigbee2mqtt/living_room/cabinet/door"
 TOPIC_CABINET_LIGHT = "zigbee2mqtt/living_room/cabinet/light"
 TOPIC_TOILET_MOTION = "zigbee2mqtt/toilet/motion"
 TOPIC_TOILET_LIGHT = "zigbee2mqtt/toilet/light"
+TOPIC_TOILET_DOOR = "zigbee2mqtt/toilet/door"
 TOPIC_BATHROOM_MOTION = "zigbee2mqtt/bathroom/motion"
 TOPIC_BATHROOM_LIGHT = "zigbee2mqtt/bathroom/light"
 
 LIGHT_OFF_CABINET = 15 * 60
-LIGHT_OFF_ROOM = 5 * 60
+LIGHT_OFF_ROOM = 1 * 60
 
 def on_connect(client, _, __, reason_code, ___):
     print(f"Connected with result code {reason_code}")
     client.subscribe(TOPIC_CABINET_DOOR)
+
     #client.subscribe(TOPIC_TOILET_MOTION)
     client.subscribe(TOPIC_TOILET_LIGHT)
+    client.subscribe(TOPIC_TOILET_DOOR)
+
     #client.subscribe(TOPIC_BATHROOM_MOTION)
     client.subscribe(TOPIC_BATHROOM_LIGHT)
 
 def on_message(_, __, msg):
     print(msg.topic+" "+str(msg.payload))
+    payload = json.loads(msg.payload)
 
     # Cabinet
     if msg.topic == TOPIC_CABINET_DOOR:
-        payload = json.loads(msg.payload)
         if "contact" in payload:
             if payload["contact"] is False:
                 cabinet_light.on()
@@ -34,24 +38,23 @@ def on_message(_, __, msg):
 
     # Toilet
     elif msg.topic == TOPIC_TOILET_MOTION:
-        payload = json.loads(msg.payload)
         if "occupancy" in payload and payload["occupancy"]:
             toilet_light.on()
     elif msg.topic == TOPIC_TOILET_LIGHT:
-        payload = json.loads(msg.payload)
         if "state" in payload:
             state = payload["state"]
             toilet_light.update_state(state)
+    # elif msg.topic == TOPIC_TOILET_DOOR:
+    #     if "contact" in payload:
+    #         toilet_light.update_door(payload["contact"])
 
     # Bathroom
     elif msg.topic == TOPIC_BATHROOM_MOTION:
-        payload = json.loads(msg.payload)
         if "illuminance_lux" in payload:
             bathroom_light.update_lux(payload["illuminance_lux"])
         if "occupancy" in payload and payload["occupancy"]:
             bathroom_light.on_bathroom()
     elif msg.topic == TOPIC_BATHROOM_LIGHT:
-        payload = json.loads(msg.payload)
         if "state" in payload:
             state = payload["state"]
             bathroom_light.update_state(state)
@@ -63,7 +66,7 @@ mqttc.on_message = on_message
 mqttc.connect("localhost", 1883, 60)
 
 cabinet_light = Light(mqttc, TOPIC_CABINET_LIGHT, LIGHT_OFF_CABINET)
-toilet_light = Light(mqttc, TOPIC_TOILET_LIGHT, LIGHT_OFF_ROOM)
+toilet_light = Light(mqttc, TOPIC_TOILET_LIGHT, LIGHT_OFF_ROOM, topic_door=TOPIC_TOILET_DOOR)
 bathroom_light = Light(mqttc, TOPIC_BATHROOM_LIGHT, LIGHT_OFF_ROOM)
 
 mqttc.loop_forever()
