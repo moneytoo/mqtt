296258779436d7fbce076533b60e144f1919e529
Marcel Dopita <m@rcel.cz>
2024-06-25 20:48:21 +0200

    Update

--- remote.py
+++ remote.py
@@ -1,10 +1,15 @@
+import json
 from utils import control_light, control_light_step
+
+STEP = 16
 
 class Remote:
     def __init__(self, client, topic_light):
         self.client = client
         self.topic_light = topic_light
         self.state = None
+
+        self.client.publish(f"{self.topic_light}/get", json.dumps({"state":""}))
 
     def update_state(self, state):
         print(f"State: {state}")
@@ -16,17 +21,17 @@ class Remote:
             control_light(self.client, self.topic_light, "toggle")
         elif action == "up_press":
             if self.state is None or self.state == "ON":
-                control_light_step(self.client, self.topic_light, 16)
+                control_light_step(self.client, self.topic_light, STEP)
             else:
-                control_light(self.client, self.topic_light, "ON", brightness=256)
+                control_light(self.client, self.topic_light, "ON", brightness=255)
         elif action == "down_press":
             if self.state is None or self.state == "ON":
-                control_light_step(self.client, self.topic_light, -16)
+                control_light_step(self.client, self.topic_light, - STEP)
             else:
                 control_light(self.client, self.topic_light, "ON", brightness=16)
         elif action == "off_press":
             control_light(self.client, self.topic_light, "ON", brightness=128)
         elif action == "up_hold":
-            control_light(self.client, self.topic_light, "ON", brightness=256)
+            control_light(self.client, self.topic_light, "ON", brightness=255)
         elif action == "down_hold":
             control_light(self.client, self.topic_light, "ON", brightness=16)
